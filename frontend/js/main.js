329f5311606a735dcebe309c5319fab9640c8a22
samiul105 <coding.samiul@gmail.com>
2025-12-08 00:07:16 +0600

    bootstrap icons replaced by font aswam icon

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -80,8 +80,8 @@
     loop: true,
     nav: true,
     navText: [
-      '<i class="bi bi-chevron-left"></i>',
-      '<i class="bi bi-chevron-right"></i>',
+      '<i class="fa-solid fa-angle-left"></i>',
+      '<i class="fa-solid fa-angle-right"></i>',
     ],
   });
 
